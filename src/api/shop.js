f06709eb7663233fa0bc7d8eb0e9ac5943845e71
Nemanja <nemanja.mitic@msgconsulting.net>
2020-11-16 14:00:11 +0100

    Add list of Products

--- src/api/shop.js
+++ src/api/shop.js
@@ -2,7 +2,7 @@ const _products = [
     {"id": 1, "title": "Koren", "price": 1150, "inventory": 200},
     {"id": 2, "title": "Ujan Nomo", "price": 570, "inventory": 100},
     {"id": 3, "title": "Medessa", "price": 740, "inventory": 150},
-    {"id": 3, "title": "Amin", "price": 860, "inventory": 50}
+    {"id": 4, "title": "Amin", "price": 860, "inventory": 50}
   ]
   
   export default {
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <ul>
+            <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import shop from "@/api/shop"
+
+export default {
+    data (){
+        return {
+            product: []
+        }
+    },
+    created(){
+        shop.getProducts(products =>{
+            this.products = products
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
