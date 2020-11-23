f20c18865807d72592ba214c4af408c34c2054be
Nemanja <nemanja.mitic@msgconsulting.net>
2020-11-23 16:06:59 +0100

    Add the new component Shopping Cart

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+        <h1>Shopping cart</h1>
+        <ul>
+            <li 
+            v-for="product in products" 
+            :key="product.id">
+            {{product.title}} - {{product.prica}} - {{product.quantity}}
+            </li>
+        </ul>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
