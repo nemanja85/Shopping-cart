ccd15dd6890f8d21d4dd27b6a21d8b2e66cb7a63
Nemanja <nemanja.mitic@msgconsulting.net>
2020-11-23 16:41:06 +0100

    Add Currency component for vue filter. Use code from vuex github account

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import {currency} from '@/Currency.vue'
 
 Vue.config.productionTip = false
+Vue.filter('currency', currency)
 
 new Vue({
   store,
